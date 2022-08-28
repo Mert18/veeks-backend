54c64713ad38c74c31879645322a3ebf5b676b26
mert18 <mertuygur02@gmail.com>
2022-08-28 09:44:28 +0300

    Spring boot initializr

---
+++ src/main/java/org/hachiko/veeks/veeks/VeeksApplication.java
@@ -0,0 +1,13 @@
+package org.hachiko.veeks.veeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeeksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hachiko/veeks/veeks/VeeksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hachiko.veeks.veeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
